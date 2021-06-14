ee2d8a752bc3f9d029da50e88b96190cd498b70b
Alexander-09 <79199006+Alexander-09@users.noreply.github.com>
2021-06-14 12:18:06 +0100

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,3 +1,18 @@
-function addUser() 
+var firebaseConfig = 
     {
+    apiKey: "AIzaSyC9lf1CVdo1cxNUA9AYkgrQnQv4Xvlo4g8",
+    authDomain: "kwitterwebapp-4882d.firebaseapp.com",
+    projectId: "kwitterwebapp-4882d",
+    storageBucket: "kwitterwebapp-4882d.appspot.com",
+    messagingSenderId: "778077558992",
+    appId: "1:778077558992:web:3660ced31ec1436c4f66d5"
+    };
+
+firebase.initializeApp(firebaseConfig);
+
+function addUser()
+    {
+    user_name = document.getElementById("user_name").value;
+
+    firebase.database().ref("/").child(user_name).update({purpose : "adding user"})
     }
